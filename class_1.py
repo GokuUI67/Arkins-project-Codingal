6df62afafe5fe031c7ea4b39529e309849fc393e
GokuUI67 <arkinpratapsingh@gmail.com>
2025-11-12 17:33:06 +0700

    class1

---
+++ class_1.py
@@ -0,0 +1,9 @@
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+sum = tree1+tree2+tree3+tree4+tree4
+print("the Sum of All 5 Trees are:...", sum)
+average = sum/5
+print("The Average of All Trees are:...", average)
