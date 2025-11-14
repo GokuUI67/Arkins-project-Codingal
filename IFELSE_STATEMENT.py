782d1ec65e8d6fedfefa7f978406acb3aac66b86
GokuUI67 <arkinpratapsingh@gmail.com>
2025-11-14 17:15:27 +0700

    ifelse_statement

---
+++ IFELSE_STATEMENT.py
@@ -0,0 +1,5 @@
+num = 3
+if num > 0:
+    print("Duuuuuuuuudddeee,this number is a positive numbeeeeeeeeeerrrr...")
+else:
+    print("Bruhhhhhhhh, this number is a negetive numbAAAAAAARARRRARRRRRRERRRRRRRRRRRRRHHHHHHH")
