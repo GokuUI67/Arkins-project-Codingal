426440131a0409c1a90d80e6d816d9490c9a5c41
GokuUI67 <arkinpratapsingh@gmail.com>
2026-01-07 17:37:56 +0700

    KRATOS

---
+++ PASS.py
@@ -0,0 +1,7 @@
+a = input ("ENTER WORD NOW:")
+for i in a:
+    if (i == 'A'):
+        print("WEEEE FOOUUUNNNDDDD AAAAAAAAAAAA")
+    else:
+        print("READ IT BOY...Weee...Did Not...FIND AAA")
+        break
