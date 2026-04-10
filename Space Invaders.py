f4b19de745f13e155f1bfc62a89dd25329d2119e
GokuUI67 <arkinpratapsingh@gmail.com>
2026-04-10 17:16:16 +0700

    TKINTESLASSSSSSSSSS     EXPENCIVE PETROLEUMMMMMMMMMMMM

--- Space Invaders.py
+++ Space Invaders.py
@@ -45,4 +45,18 @@ textX = 10
 textY = 10
 over_font = pygame.font.Font('freesansbold', 64)
 def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_blit, (200, 250))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+def isCollision(enemyX, enemyY, bulletX, bulletY):
     
---
+++ Tkinter.py
@@ -0,0 +1,5 @@
+from tkinter import *
+window = Tk()
+window.title('DEMEO WINDEOOOOOOOOOOOOOOOOOO WIMGARDEMS LEVSLISOSLAS')
+window.geometry('400x500')
+window.mainloop()
